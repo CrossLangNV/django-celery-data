81b58551d401d64190f5f0028938785580fb03fb
feveraer <frederic.everaert@gmail.com>
2019-12-02 14:30:55 +0100

    django CORS headers config

--- demo_app/settings.py
+++ demo_app/settings.py
@@ -40,10 +40,13 @@ INSTALLED_APPS = [
     'scan_app',
     'django_celery_results',
     'bootstrap4',
-    'rest_framework'
+    'rest_framework',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -52,6 +55,10 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ORIGIN_WHITELIST = (
+    'http://localhost:3000',
+)
 
 ROOT_URLCONF = 'demo_app.urls'
 
